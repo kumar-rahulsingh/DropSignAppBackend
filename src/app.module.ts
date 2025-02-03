eb3c80ec2c84d4dde0936a5b4fd4a6c0ac5de7ea
Rahul Singh <101590599+kumar-rahulsingh@users.noreply.github.com>
2025-02-03 17:04:45 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-// import { ScheduleModule } from '@nestjs/schedule';
+
 import { DropModule } from './dropsign/dropsign.module';
 import { Document } from './dropsign/dropsign.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
-    // ScheduleModule.forRoot(), 
+     
     DropModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST ,
       port: parseInt(process.env.DB_PORT || '5432', 10), 
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || 'password',
-      database: process.env.DB_NAME || 'signing_db',
+      username: process.env.DB_USER ,
+      password: process.env.DB_PASS ,
+      database: process.env.DB_NAME ,
       entities: [Document],
       synchronize: true,
       logging: true,
